7595d04ccd1bab00573bbfd01480b518bca4b10a
Chung Kao <chungckao@gmail.com>
2022-04-16 21:33:33 +0800

    Complete phase 2 of the todo app assignment

--- todosite/todoapp/admin.py
+++ todosite/todoapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Task, Note
+from .models import Task, Note, Comment, Tag
 
 # Register your models here.
 admin.site.register(Task)
 admin.site.register(Note)
+admin.site.register(Comment)
+admin.site.register(Tag)
--- todosite/todoapp/forms.py
+++ todosite/todoapp/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 
 class TaskForm(forms.Form):
-    description = forms.CharField(label='Add task', max_length=255)
+    description = forms.CharField(label='Add task', max_length=255, widget=forms.TextInput(attrs={'size': '50'}))
 
 class UpdateForm(forms.Form):
-    description = forms.CharField(label='Update task', max_length=255)
+    description = forms.CharField(label='Action', max_length=255, widget=forms.TextInput(attrs={'size': '50'}))
 
 class NoteForm(forms.Form):
-    text = forms.CharField(label='Add/Remove note', max_length=255)
+    text = forms.CharField(label='Action', max_length=255, widget=forms.TextInput(attrs={'size': '50'}))
+
+class CommentForm(forms.Form):
+    content = forms.CharField(label='', max_length=255, widget=forms.Textarea(attrs={'rows': '10', 'cols': '40'}))
+
+class TagForm(forms.Form):
+    name = forms.CharField(label='Tag', max_length=30, widget=forms.TextInput(attrs={'size': '30'}))
